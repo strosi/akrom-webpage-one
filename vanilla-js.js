117163c6ada60ecec9982a8be6e131d47ace5f23
strosi <stoeva.rositsa@gmail.com>
2020-06-30 23:59:51 +0300

    add html new content

---
+++ vanilla-js.js
@@ -0,0 +1,13 @@
+const bike = document.getElementsByClassName("bike")[0];
+const bikeBackground = document.getElementsByClassName("bike-bg")[0];
+
+function onScrollAnimation() {
+    let windowTop = window.scrollY;
+    let staticBgTop = bikeBackground.offsetTop;
+    let leftMoveValue = windowTop * 3 - staticBgTop - 120;
+
+    bike.style.left = leftMoveValue + "px";
+}
+
+window.onscroll = onScrollAnimation;
+window.onload = function () { bike.style.left = "-120px" };
